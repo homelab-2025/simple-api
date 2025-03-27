e96830e86ec7436eb82e3894e8f59b0839928a70
Enzo G <gl@enzog.mozmail.com>
2025-03-27 22:17:57 +0100

    refactor code and tests with python modules

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from main import app
+from src.main import app
 
 client = TestClient(app)
 
@@ -10,15 +10,18 @@ def test_read_root():
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
+
 def test_read_hello():
     response = client.get("/hello/User")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello User"}
 
+
 def test_read_health():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
+
 
 def test_trigger_error():
     response = client.get("/error")
